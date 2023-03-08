89d4bec0a2c92befdb1720433c3cb4489adf4314
AbdallahAbujazar <abdallah.abujazar@iths.se>
2023-03-08 13:42:27 +0200

    coding Js for product detials page and html

--- js/products.js
+++ js/products.js
@@ -71,7 +71,7 @@ addButton.addEventListener("click", function(event) {
   for (let i = 0; i < store.products.length; i++) {
     const product = store.products[i];
     const listItem = document.createElement("li");
-    listItem.innerHTML = `<a href="product-details.html?id=${product.productId}"> ${product.productName} </a> <span> Price: ${product.productPrice}</span><a href="#" class="edit-link" data-product-id="${product.productId}">Edit</a> - <a href="#"  class="delete-link" id="${product.productId}">Delete</a>`;
+    listItem.innerHTML = `<a href="product-details.htmlgit?id=${product.productId}"> ${product.productName} </a> <span> Price: ${product.productPrice}</span><a href="#" class="edit-link" data-product-id="${product.productId}">Edit</a> - <a href="#"  class="delete-link" id="${product.productId}">Delete</a>`;
     productList.appendChild(listItem);
   }
   
@@ -88,7 +88,7 @@ addButton.addEventListener("click", function(event) {
       document.getElementById("isFeaturedProduct").value = product.isFeatured ? "1" : "0";
       document.getElementById("isNewArrival").value = product.isNewArrival ? "1" : "0";
   
-      // change the add button to an edit button
+      // change the add button to an edit bu
       const addButton = document.getElementById("addButton");
       addButton.innerHTML = "Update Product";
       addButton.removeEventListener("click", addProduct); // remove the addProduct event listener
